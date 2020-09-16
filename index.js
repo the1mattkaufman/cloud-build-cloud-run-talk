71b4ccbac7eed82da0d37b2fadf803d87dc05d84
Matt Kaufman <the1mattkaufman@gmail.com>
2020-09-15 21:38:45 -0700

    name = world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  const name = process.env.NAME || "GDG Modesto";
+  const name = process.env.NAME || "World";
   res.send(`Hello ${name}!`);
 });
 
